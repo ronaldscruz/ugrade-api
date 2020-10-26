f4707e53ba1e8867e3dd4bfbc462cebbad8c9272
ronaldscruz <ronald.scruz16@gmail.com>
2020-10-26 16:09:32 -0300

    chore(commitlint): added available commit types

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,14 @@
+// Commit types:
+// - build
+// - ci
+// - chore
+// - docs
+// - feat
+// - fix
+// - perf
+// - refactor
+// - revert
+// - style
+// - test
+
 module.exports = {extends: ['@commitlint/config-conventional']}
